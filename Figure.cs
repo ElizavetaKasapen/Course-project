e80a3230ff536061a167367b7f05dcf6b8fb556c
ElizavetaKasapen <kasapen@ukr.net>
2019-04-04 19:11:55 +0300

    Create Figure.cs

---
+++ Figure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_project
+{
+    class Figure
+    {
+        bool active = false;
+        bool lady = false;
+
+        public bool Active
+        {
+            set { active = value; }
+            get { return active; }
+        }
+
+        public bool Lady
+        {
+            set { lady = value; }
+            get { return lady; }
+        }
+
+        public Figure() { }
+
+
+    }
+}
